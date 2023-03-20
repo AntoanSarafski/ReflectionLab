cf5b91d8951ebe68734ec2e60f00a0e85597a093
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-20 23:11:59 +0200

    Example how to use two promotions. How to get two values in enum with HasFlag!

---
+++ EnumBinary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EnumBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Days promotion = Days.Monday | Days.Tuesday;
+
+
+            if (promotion.HasFlag(Days.Monday))
+            {
+                Console.WriteLine("Monday Promotion");
+            }
+
+
+            if (promotion.HasFlag(Days.Tuesday))
+            {
+                Console.WriteLine("Tuesday Promotion");
+            }
+
+
+        }
+
+        enum Days
+        {
+            Monday = 0,
+            Tuesday = 2,
+            Wednesday = 4,
+            Thursday = 8,
+            Friday = 16,
+            Saturday = 32,
+            Sunday = 64
+        }
+    
+    }
+}
