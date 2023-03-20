b050ec050b22b5c13da35acaf1d22549767b3647
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-20 23:00:35 +0200

    Thats the way to read all types of fields (Non public, instance, static and public)

---
+++ GetAllFields/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace GetAllFields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Type type = typeof(Product);
+
+            FieldInfo[] fields = type.GetFields(
+                BindingFlags.NonPublic
+                | BindingFlags.Instance
+                | BindingFlags.Static
+                | BindingFlags.Public);
+
+            foreach (FieldInfo field in fields)
+            {
+                Console.WriteLine(field.Name);
+            }
+        }
+
+        class Product
+        {
+            private int privateField;
+            protected int protectedField;
+            internal int internalField;
+            public int publicField;
+
+            public static int publicStaticField;
+        }
+
+    }
+}
