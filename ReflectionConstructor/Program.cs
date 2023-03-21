b05c540fd48cbf7327142cfdb086a22e95f277aa
antoansarafski@protonmail.com <antoansarafski@protonmail.com>
2023-03-21 13:36:08 +0200

    Thats the way to give a default values of our parameters, found in constructor.

--- ReflectionConstructor/Program.cs
+++ ReflectionConstructor/Program.cs
@@ -11,11 +11,39 @@ namespace ReflectionConstructor
             Type type = typeof(Product);
             ConstructorInfo[] constructors = type.GetConstructors(); // That give us all public constructors!
 
-            Product product = (Product)Activator.CreateInstance(type, new object[] { 5, 5m, "New"}); //Reflection finds a constructor that matches to the given parameters.
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                ParameterInfo[] parameters = constructor.GetParameters();
 
-            Console.WriteLine(product.Id);
-            Console.WriteLine(product.Price);
-            Console.WriteLine(product.Model);
+
+                object[] paramValues = new object[parameters.Length];
+
+                int index = 0;
+                foreach (ParameterInfo paramInfo in parameters)
+                {
+                    paramValues[index++] = GetDefault(paramInfo.ParameterType);
+                }
+
+                Product product = Activator.CreateInstance(type, paramValues) as Product;
+
+                Console.WriteLine(product.Id);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Model);
+            }
+
+
+
+
+
+            object GetDefault(Type type)
+            {
+                if (type.IsValueType)
+                {
+                    return Activator.CreateInstance(type);
+                }
+                return null;
+            }
+
         }
     }
 }
