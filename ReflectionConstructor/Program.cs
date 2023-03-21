d83a8831ddc56098c526eb2f67b9f8859942f715
antoansarafski@protonmail.com <antoansarafski@protonmail.com>
2023-03-21 13:11:54 +0200

    Start writting example for ReflectionConstructor

---
+++ ReflectionConstructor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReflectionConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
