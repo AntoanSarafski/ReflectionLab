358e8d5b75bd20c72fe261dee951cdc6759d97a7
antoansarafski@protonmail.com <antoansarafski@protonmail.com>
2023-03-21 12:45:51 +0200

    This is the way to get properties. Is the same as to get fields.

---
+++ ReflectionProperties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PropertyInfo[] properties = typeof(Product).GetProperties((BindingFlags)60);
+
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine($"Name: {property.Name}");
+                Console.WriteLine($"Type: {property.PropertyType.Name}");
+            }
+        }
+    }
+
+    class Product
+    {
+        public int Id { get; set; }
+        public int Model { get; set; }
+    }
+}
